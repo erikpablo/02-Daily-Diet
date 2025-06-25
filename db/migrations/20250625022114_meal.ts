26d3634a80dc16bfa7fdb2048ba7ffb7832e2e38
Erik Pablo <nunespablo470@gmail.com>
2025-06-24 23:53:00 -0300

    feat: :sparkles: config database

---
+++ db/migrations/20250625022114_meal.ts
@@ -0,0 +1,19 @@
+import type { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('meal', (table) => {
+    table.uuid('id').primary()
+    table.text('name').notNullable()
+    table.text('description').notNullable()
+    table.boolean('is_diet').defaultTo(true).notNullable()
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+
+    // create the foreign key
+    table.uuid('user_id').notNullable()
+    table.foreign('user_id').references('id').inTable('users')
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTableIfExists('users')
+}
