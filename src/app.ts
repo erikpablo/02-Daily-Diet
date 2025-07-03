518511f22c299a53e97c412c1171de616c652ff0
Erik Pablo <nunespablo470@gmail.com>
2025-07-03 19:58:54 -0300

    feat: :sparkles: add new route to create a meal

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
-import fastify from 'fastify'
-import { usersRoutes } from './routes/users.router.ts'
 import cookie from '@fastify/cookie'
+import fastify from 'fastify'
+import { usersRoutes } from './routes/users.route.ts'
+import { mealsRoutes } from './routes/meals.route.ts'
 
 export const app = fastify()
 
 app.register(cookie)
 app.register(usersRoutes)
+app.register(mealsRoutes)
