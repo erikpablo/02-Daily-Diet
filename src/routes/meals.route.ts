81c482d02e6b2d546e14a76a3ccbd14a1d643fd7
Erik Pablo <nunespablo470@gmail.com>
2025-07-03 19:59:35 -0300

    feat: :sparkles: add create route a meal

---
+++ src/routes/meals.route.ts
@@ -0,0 +1,33 @@
+import type { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists.ts'
+import { knex } from '../database.ts'
+import { randomUUID } from 'node:crypto'
+
+export async function mealsRoutes(app: FastifyInstance) {
+  app.post(
+    '/meals',
+    { preHandler: [checkSessionIdExists] },
+    async (request, reply) => {
+      const { id } = request.user!
+
+      const mealSchema = z.object({
+        name: z.string(),
+        description: z.string(),
+        is_diet: z.boolean(),
+      })
+
+      const { name, description, is_diet } = mealSchema.parse(request.body)
+
+      await knex('meals').insert({
+        id: randomUUID(),
+        name,
+        description,
+        is_diet,
+        user_id: id,
+      })
+
+      return reply.status(201).send()
+    }
+  )
+}
